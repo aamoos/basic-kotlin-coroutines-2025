489950f33e2e32d3a38b6e71464ed1deac06b194
kimjaesung <aamoos@naver.com>
2025-01-21 11:04:34 +0900

    3. Cancellation and Timeouts

---
+++ src/main/kotlin/kotlinsource/cancellation/example_cancel_03.kt
@@ -0,0 +1,31 @@
+package kotlinsource.cancellation
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    val startTime = System.currentTimeMillis()
+    val job = launch (Dispatchers.Default) {
+
+        try{
+            var nextPrintTime = startTime
+            var i = 0
+            while(isActive){
+                if(System.currentTimeMillis() >= nextPrintTime){
+                    // delay(1L)
+                    // yield()
+                    println("job: I'm sleeping ${i++} ...")
+                    nextPrintTime += 500L
+                }
+            }
+            kotlin.io.println("isActive $isActive ...")
+        }catch (e: Exception){
+            kotlin.io.println("Exception [$e]")
+        }
+    }
+
+    delay(1300L)
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin()
+    println("main: Now I can quit.")
+}
